aa4cc9e512763c837e5735816288022007098e25
Flavio Lopes <flaviotxa@gmail.com>
2021-04-18 20:59:38 +0100

    Load and display author data via Redux flow

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import courses from "./courseReducer";
+import authors from "./authorReduces";
 
 const rootReducer = combineReducers({
   courses,
+  authors,
 });
 
 export default rootReducer;
