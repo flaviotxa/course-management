8aa8b9fe523610ca3a1b35ffc14f90354bc6b849
Flavio Lopes <flaviotxa@gmail.com>
2021-04-22 21:54:21 +0100

    Configured production environment

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,34 +1,50 @@
 const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpackBundleAnalyzer = require("webpack-bundle-analyzer");
 
-process.env.NODE_ENV = "development";
+process.env.NODE_ENV = "production";
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   target: "web",
-  devtool: "cheap-module-source-map",
+  devtool: "source-map",
   entry: "./src/index",
   output: {
     path: path.resolve(__dirname, "build"),
     publicPath: "/",
     filename: "bundle.js",
   },
-  devServer: {
-    stats: "minimal",
-    overlay: true,
-    historyApiFallback: true,
-    disableHostCheck: true,
-    headers: { "Access-Control-Allow-Origin": "*" },
-    https: false,
-  },
   plugins: [
+    // Display bundle stats
+    new webpackBundleAnalyzer.BundleAnalyzerPlugin({ analyzerMode: "static" }),
+
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+
     new webpack.DefinePlugin({
+      // This global makes sure React is built in prod mode.
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
       "process.env.API_URL": JSON.stringify("http://localhost:3001"),
     }),
     new HtmlWebpackPlugin({
       template: "src/index.html",
       favicon: "src/favicon.ico",
+      minify: {
+        // see https://github.com/kangax/html-minifier#options-quick-reference
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
     }),
   ],
   module: {
@@ -40,7 +56,22 @@ module.exports = {
       },
       {
         test: /(\.css)$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => [require("cssnano")],
+              sourceMap: true,
+            },
+          },
+        ],
       },
     ],
   },
